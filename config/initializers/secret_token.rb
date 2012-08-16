78437d6017ae56f7fae9f482b7694c5f250f3989
Matt Diebolt <mdiebolt@gmail.com>
2012-08-15 21:57:59 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InStock::Application.config.secret_token = 'c1e73f215b5eb2258fedad4c77bcdfa2180032f0d6b69885b0587b94d652e1cd18d7091fe7b376a6c39747ef2d0424baf9b2c30b373fb718ad54a88f64d45f51'
